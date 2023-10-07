5f1763a8c17dc533af984e34a5d25edffc0c6101
BartoszJO <bartoszjolszewski@hotmail.com>
2023-10-07 18:13:12 +0200

    First Commit

---
+++ src/main/java/com/kursjava/maven/multimod/App.java
@@ -0,0 +1,13 @@
+package com.kursjava.maven.multimod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
